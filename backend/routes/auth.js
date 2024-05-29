d752c73382d763a757a3e5729328e227444c6158
ssrivastava098 <shubhamsrivastava290321@gmail.com>
2024-05-29 14:54:38 +0530

    After video no 46 Express Validator
    In this commit I have implemented Express Validator to validate the inputs gong into our database.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 //Create a USer using: POST "app/auth/". Doesn't require Auth
 
-router.post("/", (req, res) => {
-    res.send(req.body)
-    const user= User(req.body)
-    user.save()
-    console.log(req.body)
+router.post("/", [
+    // Validation rules
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        res.send(req.body)
+        const { name, email, password } = req.body;
+        User.create({ name, email, password });
+        // const user= User(req.body)
+        // user.save()
+        console.log(req.body)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
 })
 
 module.exports = router;
